ec490a97bc04bc7e0c825b17991dd07c52eca265
ETH CyberGoth (CyberGoth) <mr.grass0420@gmail.com>
2026-01-30 03:15:50 +0000

    还是有白线，继续移除

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,11 @@
-import { PromoCarousel } from '@/components/promo-carousel';
-import { NearbyRecommendations } from '@/components/nearby-recommendations';
-import { SearchBar } from '@/components/layout/search-bar';
-import Link from 'next/link';
+// All components have been removed from this page to debug a persistent white line issue.
 
 export default async function HomePage() {
-  const hotSearches = ["热水袋", "羽绒服", "电动车", "男童羽绒服", "巧克力", "手机壳", "女衣", "牛仔裤"];
-
   return (
     <div className="flex flex-col">
-      {/* The PromoCarousel section has been removed for debugging the white line issue. */}
-      
       <div className="container mx-auto px-4">
-        {/* The SearchBar section has been removed for debugging the white line issue. */}
-
         <div className="pb-12">
-          {/* This is the AI powered recommendation section, which is now the main product grid */}
-          <NearbyRecommendations />
+          {/* All components removed for debugging */}
         </div>
       </div>
     </div>
