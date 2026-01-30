971ddd66e168d6b3646ed422cb8518390a29c6d7
ETH CyberGoth (CyberGoth) <mr.grass0420@gmail.com>
2026-01-30 20:36:18 +0000

    依旧没有，你删除掉所有关于10%的无效代码把，剩下那10%你就用纯白色呼吸灯来表示，让人感觉这10%完成了才是交易成功。

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -12,7 +12,7 @@ const Progress = React.forwardRef<
   <ProgressPrimitive.Root
     ref={ref}
     className={cn(
-      "relative h-4 w-full overflow-hidden bg-secondary",
+      "relative h-4 w-full overflow-hidden bg-secondary animate-breathing-glow",
       className
     )}
     {...props}
