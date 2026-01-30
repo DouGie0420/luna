c42169316ced7ab7c6e0101f6a5a354368a9faba
ETH CyberGoth (CyberGoth) <mr.grass0420@gmail.com>
2026-01-30 03:16:56 +0000

    还是有白线，继续移除

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,6 @@
 // All components have been removed from this page to debug a persistent white line issue.
+// Now removing container divs as well.
 
 export default async function HomePage() {
-  return (
-    <div className="flex flex-col">
-      <div className="container mx-auto px-4">
-        <div className="pb-12">
-          {/* All components removed for debugging */}
-        </div>
-      </div>
-    </div>
-  );
+  return null;
 }
