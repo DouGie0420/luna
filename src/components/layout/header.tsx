ee59b9be8b08f3376f2e864ed2f86f28208ef3b1
ETH CyberGoth (CyberGoth) <mr.grass0420@gmail.com>
2026-01-30 01:03:30 +0000

    我需要你和我的对话全程为中文。我现在没看到我这个网站的搜索功能，这个功能应该在最显眼的地方，因为这个网站的目的是为了让人发布和购买商品

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -2,24 +2,24 @@ import { Logo } from "./logo";
 import { MainNav } from "./main-nav";
 import { UserNav } from "./user-nav";
 import { SearchBar } from "./search-bar";
-import { Button } from "../ui/button";
 
 export function Header() {
   return (
     <header className="sticky top-0 z-40 w-full border-b bg-background">
-      <div className="container mx-auto flex h-16 items-center space-x-4 px-4 sm:justify-between sm:space-x-0">
-        <div className="flex gap-6 md:gap-10">
+      <div className="container mx-auto flex h-16 items-center gap-4 px-4">
+        <div className="flex items-center gap-6 md:gap-10">
           <Logo />
           <MainNav />
         </div>
 
-        <div className="flex flex-1 items-center justify-end space-x-4">
-          <div className="flex flex-1 max-w-xs md:max-w-sm">
+        <div className="flex-1 px-4">
+          <div className="mx-auto w-full max-w-md">
             <SearchBar />
           </div>
-          <nav className="flex items-center space-x-2">
-            <UserNav />
-          </nav>
+        </div>
+
+        <div className="flex items-center">
+          <UserNav />
         </div>
       </div>
     </header>
