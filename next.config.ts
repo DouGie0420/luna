d9b78f76c7b90e5f3b924d23d6aa0a328e790789
ETH CyberGoth (CyberGoth) <mr.grass0420@gmail.com>
2026-01-31 23:02:15 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://witches

--- next.config.ts
+++ next.config.ts
@@ -53,6 +53,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'witchesnft.xyz',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
   devIndicators: {
