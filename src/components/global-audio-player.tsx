f81482cba492618d1601f40493b413067ae3097c
ETH CyberGoth (CyberGoth) <mr.grass0420@gmail.com>
2026-02-08 23:18:28 +0000

    请将播放器中的图片链接替换成：https://photos.google.com/album/AF1QipOuhrA2anUcJoGIZoQgM

--- src/components/global-audio-player.tsx
+++ src/components/global-audio-player.tsx
@@ -138,7 +138,7 @@ export function GlobalAudioPlayer() {
             <div className="flex h-full items-center">
               <div className="relative w-16 h-16 rounded-lg overflow-hidden border border-white/10 shrink-0">
                 <img 
-                  src={channelIcon || 'https://picsum.photos/seed/lunaradio/64/64'} 
+                  src={channelIcon || 'https://picsum.photos/seed/red_cyber/64/64'} 
                   className="object-cover w-full h-full" 
                   alt={stationName || 'Luna Labs Radio'} 
                 />
