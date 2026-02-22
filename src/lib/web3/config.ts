19001886677ef149246e4f2acffa8d2bf83457a4
CyberGoth <mr.grass0420@gmail.com>
2026-02-22 10:06:03 +0000

    Luna 平台合约部署及权限配置指令 请按照以下配置将部署好的 Base Sepolia 合约集成到 Firebase 系统中：
    1. 核心资

--- src/lib/web3/config.ts
+++ src/lib/web3/config.ts
@@ -3,10 +3,9 @@
 // 2. Copy the DEPLOYED CONTRACT ADDRESS and paste it into the variable below.
 // 3. From the Remix 'Solidity Compiler' tab, copy the ABI and paste it into the array below.
 
-export const escrowContractAddress = "YOUR_DEPLOYED_CONTRACT_ADDRESS";
+export const escrowContractAddress = "0x2959F9b75130ccD3543410f4aDC7FD20DBf88fDB";
+export const usdtContractAddress = "0x25E733989D7a3782989bEaB08221E237C9232215";
 
 export const escrowContractABI = [
   // PASTE_YOUR_ABI_HERE
 ];
-
-    
